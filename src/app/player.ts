8fa134e90dc06cf0cb2ed3f875ef561af37c8db6
Wenbo-Ge <wge19920921@gmail.com>
2018-06-16 17:43:43 -0400

    updates Y:wq :wq Please enter the commit message for your changes. Lines starting

---
+++ src/app/player.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Wenbo on 2018/6/13.
+ */
+export class Player {
+    id: number;
+    name: string;
+}
---
+++ src/app/players/players.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+import { PlayerService } from '../player.service';
+
+
+@Component({
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css']
+})
+export class PlayersComponent implements OnInit {
+  selectedPlayer: Player;
+  players: Player[];
+  constructor(private playerService: PlayerService) { }
+  onSelect(player: Player): void {
+    this.selectedPlayer = player;
+  }
+  getPlayers(): void {
+    this.playerService.getPlayers().subscribe(players => this.players = players);
+  }
+  ngOnInit() {
+    this.getPlayers();
+  }
+}
